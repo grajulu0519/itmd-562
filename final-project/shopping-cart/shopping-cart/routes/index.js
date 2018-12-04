95ee78ae453a10d233d1b2600a2431a349b2802d
Gauri GovindRajulu <Gauri.GovindRajulu@deltak-innovation.com>
2018-12-03 23:08:49 -0600

    added payment functionality

--- final-project/shopping-cart/shopping-cart/routes/index.js
+++ final-project/shopping-cart/shopping-cart/routes/index.js
@@ -9,6 +9,7 @@ var Product = require('../models/product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+var successMsg = req.flash('success')[0];
 Product.find(function(err,docs){
   var productChunk = [];
 
@@ -16,15 +17,13 @@ Product.find(function(err,docs){
   for(var i = 0; i < docs.length; i+= chunkSize){
     productChunk.push(docs.slice(i,i + chunkSize));
   }
-  res.render('shop/index', { title: 'Shopping Cart', products: productChunk });
+  res.render('shop/index', { title: 'Shopping Cart', products: productChunk, suucessMsg: successMsg, noMessages: !successMsg});
 });
  
 });
 
 router.get('/add-to-cart/:id', function(req, res, next){
   var productId = req.params.id;
-  console.log('inside the add to cart function');
-  console.log('req.session '+req.session);
   var cart = new Cart(req.session.cart ? req.session.cart : {} );
   Product.findById(productId, function(err, product){
     if(err){
@@ -37,6 +36,21 @@ router.get('/add-to-cart/:id', function(req, res, next){
   });
 });
 
+router.get('/reduce/:id' , function(req, res, next){
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {} );
+  cart.removeItem(productId);
+  req.session.cart = cart;
+  res.redirect('/shopping-cart');
+});
+
+router.get('/remove/:id' , function(req, res, next){
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {} );
+  cart.reduceByOne(productId);
+  req.session.cart = cart;
+  res.redirect('/shopping-cart');
+});
 router.get('/shopping-cart', function(req, res, next){
   if(!req.session.cart){
     return res.render('shop/shopping-cart', {products: null});
@@ -51,6 +65,32 @@ router.get('/checkout', function(req,res, next){
     return res.redirect('/shopping-cart');
   }
   var cart = new Cart(req.session.cart);
-  res.render('shop/checkout', {total: cart.totalPrice});
+  var errMsg = req.flash('error')[0];
+  res.render('shop/checkout', {total: cart.totalPrice, errMsg: errMsg, noError: !errMsg});
+});
+
+router.post('/checkout', function(req, res, next){
+  if(!req.session.cart){ 
+    return res.redirect('/shopping-cart');
+  }
+  var cart = new Cart(req.session.cart); 
+
+  var stripe = require("stripe")("sk_test_l58uYMMiUrFF2H0s1De2eIQY");
+
+  stripe.charges.create({
+  amount: cart.totalPrice * 100,
+  currency: "usd",
+  source: req.body.stripeToken, // obtained with Stripe.js
+  description: "Test Charge"
+}, function(err, charge) {
+  if(err){
+    req.flash('error', err.message);
+    return res.redirect('/checkout');
+  }
+  req.flash('success', 'Successfully bought product');
+  req.session.cart = null;
+  res.redirect('/');
+  
+});
 });
 module.exports = router;
